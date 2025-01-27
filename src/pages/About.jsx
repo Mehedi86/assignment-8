242bfcb308713cd188af882717dec64da5ccc0d8
Mehedi86 <abdullahalmehedi@gmail.com>
2025-01-27 18:03:21 +0600

    added: final fix

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,26 @@
 const About = () => {
     return (
-        <div>
-            This is about page
+        <div className="flex h-[600px]">
+            <div className="p-12 w-3/12">
+                <h1 className="text-2xl font-bold my-4">Physical Store</h1>
+                <p className="text-xl font-semibold p-2 my-2 bg-white rounded-xl">Dhaka</p>
+                <p className="text-xl font-semibold p-2 my-2 bg-white rounded-xl">Chittagong</p>
+                <p className="text-xl font-semibold p-2 my-2 bg-white rounded-xl">Khulna</p>
+                <p className="text-xl font-semibold p-2 my-2 bg-white rounded-xl">Rajshahi</p>
+                <p className="text-xl font-semibold p-2 my-2 bg-white rounded-xl">Sylhet</p>
+                <p className="text-xl font-semibold p-2 my-2 bg-white rounded-xl">Rangpur</p>
+            </div>
+            <div className="p-12 w-6/12">
+            <h1 className="text-2xl font-bold my-4">About Us</h1>
+            <div className="bg-white rounded-xl">
+                <p className="text-xl text-neutral-400 p-2 my-2">We have electronics eccsories form different brand and different category. We want to ecclarate our collection to gather electronics item. Now we have some category item in our inventory which already displayed in our website. We have electronics eccsories form different brand and different category. We want to ecclarate our collection to gather electronics item. Now we have some category item in our inventory which already displayed in our website. We have electronics eccsories form different brand and different category. We want to ecclarate our collection to gather electronics item. Now we have some category item in our inventory which already displayed in our website.</p>
+            </div>
+            </div>
+            <div className="p-12">
+            <h1 className="text-2xl font-bold my-4">Feel Free to contact with us</h1>
+            <p className="text-xl p-2 my-2 ">Email: www.gadget8376gmail.com</p>
+            <p className="text-xl p-2 my-2 ">Phone: 49357324975</p>
+            </div>
         </div>
     );
 };
